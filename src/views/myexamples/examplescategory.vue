e5a5ab686be51477d35b0814f0ada1f122f95130
shuaishuaidaxia <1619574445@qq.com>
2021-07-22 16:08:47 +0800

    新增examples实例-折线图和柱形图

---
+++ src/views/myexamples/examplescategory.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="mycategoy" style="width: 700px;height:400px;">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'examplescategory',
+  mounted () {
+    this.drawcategoy()
+  },
+  methods: {
+    drawcategoy () {
+      const echarts = require('echarts')
+      const mycategoy = echarts.init(document.getElementById('mycategoy'))
+      mycategoy.setOption(
+        {
+          xAxis: {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+          },
+          yAxis: {
+            type: 'value'
+          },
+          series: [{
+            data: [120, 200, 150, 80, 70, 110, 130],
+            type: 'bar',
+            showBackground: true,
+            backgroundStyle: {
+              color: 'rgba(180, 180, 180, 0.2)'
+            }
+          }]
+        }
+      )
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
